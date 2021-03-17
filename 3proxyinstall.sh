fc5a91ce4273036736559e5d8ba563dd75b506fd
merlinjk <60071741+merlinjk@users.noreply.github.com>
2021-03-17 17:48:31 +0100

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,5 +1,6 @@
-apt install make
-wget https://github.com/z3APA3A/3proxy/releases/download/0.9.3/3proxy-0.9.3.tar.gz
+apt-get update && apt-get -y upgrade
+apt-get install gcc make git -y
+wget --no-check-certificate -O https://github.com/z3APA3A/3proxy/releases/download/0.9.3/3proxy-0.9.3.tar.gz
 tar xzf 3proxy-0.9.3.tar.gz
 cd 3proxy-0.9.3
 make -f Makefile.Linux
@@ -7,10 +8,10 @@ cd bin
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget https://raw.githubusercontent.com/merlinjk/myst/main/3proxy.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/merlinjk/myst/main/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 mkdir /var/log/3proxy/
 cd /etc/init.d/
-wget --no-check-certificate  https://raw.github.com/SnoyIatk/3proxy/master/3proxy
+wget --no-check-certificate https://raw.github.com/SnoyIatk/3proxy/master/3proxy
 chmod  +x /etc/init.d/3proxy
 update-rc.d 3proxy defaults
